e54720f7e3c12ce2f7000fbc33865f938712730d
bobby569 <zhan2175@purdue.edu>
2017-12-09 17:38:02 -0500

    code clean up

--- src/components/App.js
+++ src/components/App.js
@@ -26,44 +26,39 @@ export default class App extends Component {
 	}
 
 	onDismiss(id) {
-		const hits = this.state.result.hits.filter(item => {
+		const { result } = this.state;
+		const hits = result.hits.filter(item => {
 			return item.objectID !== id;
 		});
-		this.setState({ result: { ...this.state.result, hits } });
-	}
-
-	onSearchChange(searchTerm) {
-		this.setState({ searchTerm });
-		_.debounce(term => {
-			this.fetchSearchTopStories(term);
-		}, 500)(searchTerm);
+		this.setState({ result: { ...result, hits } });
 	}
 
 	setSearchTopStories(result) {
 		const { hits, page } = result;
 		const oldHits = page !== 0 ? this.state.result.hits : [];
 		const updatedHits = [...oldHits, ...hits];
-		this.setState({
-			result: { hits: updatedHits, page }
-		});
+		this.setState({ result: { hits: updatedHits, page } });
 	}
 
 	fetchSearchTopStories(searchTerm, page = 0) {
 		axios
 			.get(`https://hn.algolia.com/api/v1/search?query=${searchTerm}&page=${page}`)
-			.then(res => {
-				this.setSearchTopStories(res.data);
-			})
+			.then(res => this.setSearchTopStories(res.data))
 			.catch(err => this.setState({ err }));
+	}
+
+	onSearchChange(searchTerm) {
+		this.setState({ searchTerm });
+
+		_.debounce(term => {
+			this.fetchSearchTopStories(term);
+		}, 500)(searchTerm);
 	}
 
 	render() {
 		const { err, result, searchTerm } = this.state;
-		if (err) {
-			return <p>Something went wrong</p>;
-		}
 		const center = { textAlign: 'center' };
-		const page = (result && result.page) || 0;
+		const page = result ? result.page : 0;
 
 		return (
 			<div className="page">
@@ -71,7 +66,7 @@ export default class App extends Component {
 				<SearchBar value={searchTerm} onSearchChange={this.onSearchChange} />
 				{err ? (
 					<div style={center}>
-						<p>Something went wrong</p>
+						<h2>Something went wrong</h2>
 					</div>
 				) : (
 					result && <Table list={result.hits} onDismiss={this.onDismiss} />
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class SearchBar extends Component {
-	render() {
-		const { value } = this.props;
-		const inputStyle = { width: '40%', textAlign: 'center' };
-
-		return (
-			<div className="search-bar">
-				<input
-					type="text"
-					value={value}
-					style={inputStyle}
-					onChange={e => this.props.onSearchChange(e.target.value)}
-					placeholder="Search"
-				/>
-			</div>
-		);
-	}
-}
+export default props => (
+	<div className="search-bar">
+		<input
+			type="text"
+			value={props.value}
+			style={{ width: '40%', textAlign: 'center' }}
+			onChange={e => props.onSearchChange(e.target.value)}
+			placeholder="Search"
+		/>
+	</div>
+);
